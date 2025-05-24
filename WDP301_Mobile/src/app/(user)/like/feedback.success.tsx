798fec427322a8811c571a05575c030be71f096e
Le Minh Duy <duy.leminh.se@gmail.com>
2025-05-24 08:49:56 +0700

    Fix some bugs

--- WDP301_Mobile/src/app/(user)/like/feedback.success.tsx
+++ WDP301_Mobile/src/app/(user)/like/feedback.success.tsx
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: APP_COLOR.WHITE,
+    backgroundColor: APP_COLOR.BACKGROUND_ORANGE,
   },
   contentContainer: {
     alignItems: "center",
