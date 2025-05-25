45d535cbee9afe292273fa8cc729d1a344cafc54
Darkvolcano <khangphamchuchi@gmail.com>
2025-05-25 15:33:27 +0700

    update calculate shipping when input address

---
+++ WDP301_FE/src/hooks/ordersApi.ts
@@ -0,0 +1,54 @@
+import { useMutation } from "@tanstack/react-query";
+import axiosInstance from "../config/axios";
+
+interface CreateOrder {
+  orderItems: ProductItemDto[];
+  order_discount_value: number;
+  order_shipping_fee: number;
+  payment_method_id: number;
+  order_address: string;
+  note: string;
+}
+
+interface ProductItemDto {
+  productId: number;
+  quantity: number;
+  price: number;
+}
+
+export interface OrderReponseDto {
+  message: string;
+  orderId: number;
+  checkoutUrl: string;
+}
+
+interface CalculateShipping {
+  deliver_address: string;
+}
+
+export interface ShippingResponseDto {
+  status: number;
+  message: string;
+  fee: number;
+}
+
+export const useCreateOrder = () => {
+  return useMutation({
+    mutationFn: async (newOrder: CreateOrder) => {
+      const response = await axiosInstance.post(`orders`, newOrder);
+      return response.data;
+    },
+  });
+};
+
+export const useCalculateShipping = () => {
+  return useMutation({
+    mutationFn: async (calculateShip: CalculateShipping) => {
+      const response = await axiosInstance.post(
+        `orders/shipping/calculate`,
+        calculateShip
+      );
+      return response.data as ShippingResponseDto;
+    },
+  });
+};
