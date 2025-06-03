affb937a0725888699e8fac4ce15fd40ac11f624
Le Minh Duy <duy.leminh.se@gmail.com>
2025-06-03 14:20:06 +0700

    Top Collection List

--- WDP301_Mobile_PosApp/src/app/(auth)/homepage.tsx
+++ WDP301_Mobile_PosApp/src/app/(auth)/homepage.tsx
@@ -1,13 +1,18 @@
 import TopHeader from "@/components/headerComponent/topHeader";
+import TopList from "@/components/headerComponent/topList";
 import SearchComponent from "@/components/headerComponent/topSearch";
 import { APP_COLOR } from "@/constants/Colors";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomePage = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: APP_COLOR.WHITE }}>
-      <TopHeader />
-      <SearchComponent />
+      <View>
+        <TopHeader />
+        <SearchComponent />
+        <TopList />
+      </View>
     </SafeAreaView>
   );
 };
