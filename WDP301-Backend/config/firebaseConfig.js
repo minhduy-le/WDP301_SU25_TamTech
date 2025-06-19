8840f493493f28039eba34ff93f3273c06195392
totrieuvy <vytotrieu12a121@gmail.com>
2025-06-19 14:20:10 +0700

    test commit

--- WDP301-Backend/config/firebaseConfig.js
+++ WDP301-Backend/config/firebaseConfig.js
@@ -1,11 +1,10 @@
 // WDP301-Backend/config/firebaseConfig.js
 
-const serviceAccount = require("../utils/serviceAccountKey.json");
+// const serviceAccount = require("../utils/serviceAccountKey.json");
 
 // Thay thế ID cũ bằng Project ID mới của bạn ở đây
 const storageBucket = "notification-eef47.appspot.com";
 
 module.exports = {
-  serviceAccount,
   storageBucket,
 };
